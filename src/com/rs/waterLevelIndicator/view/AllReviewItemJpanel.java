edaf11134174d711ce88eb4277a15a61edad6db5
xiez <xiez@suchness.com>
2018-07-03 19:58:42 +0800

    增加了设备数的,设备数这个界面应该可以单独摘出来，调试，历史数据查询，测站管理，全局总揽也会用到

--- src/com/rs/waterLevelIndicator/view/AllReviewItemJpanel.java
+++ src/com/rs/waterLevelIndicator/view/AllReviewItemJpanel.java
@@ -6,20 +6,33 @@ package com.rs.waterLevelIndicator.view;
 
 import javax.swing.*;
 import javax.swing.GroupLayout;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /**
  * @author xziea
  */
-public class AllReviewItemJpanel extends JPanel {
+public class AllReviewItemJpanel extends JPanel implements ActionListener {
+    boolean isDevSelected = false;
     public AllReviewItemJpanel() {
+        initPreData();
         initComponents();
+    }
+
+    private void initPreData() {
+
     }
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         tabbedPane2 = new JTabbedPane();
         scrollPane1 = new JScrollPane();
-        mAllDevTree = new JTree();
         tabbedPane3 = new JTabbedPane();
         scrollPane2 = new JScrollPane();
         mRealTime = new JTable();
@@ -35,13 +48,99 @@ public class AllReviewItemJpanel extends JPanel {
         mSetPara = new JButton();
         panel2 = new JPanel();
 
+        //右键菜单：
+        JPopupMenu popMenu; //菜单
+        JMenuItem mDataSelectItem;//数据查询
+        JMenuItem mDevInfoItem;//设备信息
+        popMenu = new JPopupMenu();
+        mDataSelectItem = new JMenuItem("数据查询");
+        mDevInfoItem = new JMenuItem("设备信息");
+        mDataSelectItem.addActionListener(this);
+        mDevInfoItem.addActionListener(this);
+        popMenu.add(mDataSelectItem);
+        popMenu.add(mDevInfoItem);
         //======== this ========
 
         //======== tabbedPane2 ========
         {
 
             //======== scrollPane1 ========
-            {
+            { //树的初始化
+                DefaultMutableTreeNode node1 = new DefaultMutableTreeNode("检测系统");
+                DefaultMutableTreeNode node2 = new DefaultMutableTreeNode("区域");
+                DefaultMutableTreeNode node3 = new DefaultMutableTreeNode("单位");
+                DefaultMutableTreeNode node4 = new DefaultMutableTreeNode("测点");
+                node3.add(node4);
+                node2.add(node3);
+                node1.add(node2);
+                mAllDevTree = new JTree(node1);
+                mAllDevTree.setAutoscrolls(true);
+                //右键点击菜单
+                mAllDevTree.addMouseListener(new MouseListener() {
+                    @Override
+                    public void mouseClicked(MouseEvent me) {
+                        if(me.getButton() == MouseEvent.BUTTON3){
+//                            System.out.println("isDevSelected :"+isDevSelected);
+//                            if(isDevSelected)
+//                                isDevSelected = false;
+//                            popMenu.show(mAllDevTree,e.getX(), e.getY());
+                            int n=mAllDevTree.getRowForLocation(me.getX(),me.getY());
+                            if(n<0) return ;
+                            TreePath selTree=mAllDevTree.getPathForRow(n);
+                            DefaultMutableTreeNode selNode=(DefaultMutableTreeNode)selTree.getLastPathComponent();//拿到的是叶子节点
+
+                            System.out.println("selNode is :"+ selNode);//叶子节点的名称 也就是设备测点 拿到的就是设备信息
+                            if(selNode.isLeaf()){
+                                popMenu.show(mAllDevTree,me.getX(), me.getY());
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void mousePressed(MouseEvent e) {
+
+                    }
+
+                    @Override
+                    public void mouseReleased(MouseEvent e) {
+
+                    }
+
+                    @Override
+                    public void mouseEntered(MouseEvent e) {
+
+                    }
+
+                    @Override
+                    public void mouseExited(MouseEvent e) {
+
+                    }
+                });
+
+//                //设置树的左键监听
+//                mAllDevTree.addTreeSelectionListener(new TreeSelectionListener() {
+//                    @Override
+//                    public void valueChanged(TreeSelectionEvent e) {
+//                        TreePath[] paths = e.getPaths();
+//
+//                        //最后点击的是不是测点
+//                        if( paths[paths.length - 1].equals("测点")){
+//                            isDevSelected = true;
+//                        }
+////                        for (int i =0;i<paths.length;i++){
+////
+////                            if(paths.length == 1){
+////                                isDevSelected = true;
+////                            }
+////                            if (e.isAddedPath(i)){
+////                                System.out.println("path is :"+e.getPath());
+////                                isDevSelected = true;
+////                            }else {
+////
+////                            }
+//                        }
+////                    }
+//                });
                 scrollPane1.setViewportView(mAllDevTree);
             }
             tabbedPane2.addTab("\u5168\u90e8\u70b9\u4f4d", scrollPane1);
@@ -195,5 +294,10 @@ public class AllReviewItemJpanel extends JPanel {
     private JButton mReadPara;
     private JButton mSetPara;
     private JPanel panel2;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
